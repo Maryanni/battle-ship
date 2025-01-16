8e6fa0649e645c25fdf6e9838bb84bd8909aa98a
Maryanni <marziliano14n@gmail.com>
2025-01-16 20:19:07 -0300

    Third change: count my flota

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 
 function Table({ startGame, selectAble }) {
@@ -19,12 +19,18 @@ function Table({ startGame, selectAble }) {
   const tableData = myTable();
 
   const [selectCells, setSelectCells] = useState([]);
+  const [sequences, setSequences] = useState({
+    two: 0,
+    three: 0,
+    four: 0,
+    five: 0,
+  });
 
   const handleCell = (row, col) => {
     if (startGame && selectAble) {
-    const cellKey = `${row}-${col}`;
-    console.log(`Celda clicada: ${letters[row]}${col + 1}`);
-    setSelectCells((prev) =>
+      const cellKey = `${row}-${col}`;
+      console.log(`Celda clicada: ${letters[row]}${col + 1}`);
+      setSelectCells((prev) =>
         prev.includes(cellKey)
           ? prev.filter((cell) => cell !== cellKey)
           : [...prev, cellKey]
@@ -32,6 +38,54 @@ function Table({ startGame, selectAble }) {
     }
   };
 
+ 
+
+  const verifySequence = (cells) => {
+    const sortedCells = cells.map((cell) => {
+      const [row, col] = cell.split("-");
+      return { x: parseInt(row), y: parseInt(col) };
+    })
+    .sort((a, b) => a.x - b.x || a.y - b.y);
+    return findSequences(sortedCells);
+  };
+
+  const findSequences = (sortedCells) => {
+    const sequences = [];
+    let currentSequence = [sortedCells[0]];
+
+    for (let i = 1; i < sortedCells.length; i++) {
+      const prev = sortedCells[i - 1];
+      const curr = sortedCells[i];
+
+      if(
+        (prev.x === curr.x && prev.y + 1 === curr.y) ||
+        (prev.y === curr.y && prev.x + 1 === curr.x)
+      ) {
+        currentSequence.push(curr);
+      } else {
+        sequences.push(currentSequence);
+        currentSequence = [curr];
+      }
+    }
+    sequences.push(currentSequence);
+
+    const counts = { two: 0, three: 0, four: 0, five: 0 };
+    sequences.forEach((seq) => {
+      const length = seq.length;
+      if (length >= 2 && length <= 5) {
+        counts[length === 2 ? "two" : length === 3 ? "three" : length === 4 ? "four" : "five"]++;
+      }
+    });
+    return counts;
+  };
+
+  useEffect(() => {
+    if (selectCells.length > 0) {
+        const newSequences = verifySequence(selectCells);
+        console.log("Secuencias calculadas:", newSequences);
+        setSequences(newSequences); 
+      }
+  }, [selectCells]);
 
   return (
     <div className="d-flex justify-content-center">
@@ -57,7 +111,6 @@ function Table({ startGame, selectAble }) {
                 return (
                   <td
                     key={cellIndex}
-
                     className={`table-cell ${
                       startGame ? "cell-enabled" : "cell-disabled"
                     } ${isSelected ? "cell-selected-matrix" : ""}`}
